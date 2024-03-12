af7031e852ab374e9266c7cd4994bc9749a810bb
Uzair Majeed <uzairmajeed@uzairmajeeds-MacBook-Pro.local>
2024-03-12 08:31:39 +0530

    FirebasePerformance_Included

---
+++ reels/src/main/java/com/facebook/reels_project/ReelsFragment.kt
@@ -0,0 +1,94 @@
+package com.facebook.reels_project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.facebook.reels_project.databinding.ReelsactivityBinding
+
+class ReelsFragment : Fragment() {
+
+    private lateinit var binding: ReelsactivityBinding
+
+    private lateinit var adapter: VideoAdapter
+    private val exoPlayerItems = ArrayList<ExoPlayerItem>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = ReelsactivityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.backbutton.setOnClickListener {
+            requireActivity().finish()
+        }
+
+        // Retrieve videos from arguments
+        val videos: ArrayList<Video>? = arguments?.getSerializable("videos") as? ArrayList<Video>
+
+        adapter = VideoAdapter(requireContext(), videos ?: ArrayList(), object : VideoAdapter.OnVideoPreparedListener {
+            override fun onVideoPrepared(exoPlayerItem: ExoPlayerItem) {
+                exoPlayerItems.add(exoPlayerItem)
+            }
+        })
+
+        binding.viewPager2.adapter = adapter
+
+        binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                val previousIndex = exoPlayerItems.indexOfFirst { it.exoPlayer.isPlaying }
+                if (previousIndex != -1) {
+                    val player = exoPlayerItems[previousIndex].exoPlayer
+                    player.pause()
+                    player.playWhenReady = false
+                }
+                val newIndex = exoPlayerItems.indexOfFirst { it.position == position }
+                if (newIndex != -1) {
+                    val player = exoPlayerItems[newIndex].exoPlayer
+                    player.playWhenReady = true
+                    player.play()
+                }
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val index = exoPlayerItems.indexOfFirst { it.position == binding.viewPager2.currentItem }
+        if (index != -1) {
+            val player = exoPlayerItems[index].exoPlayer
+            player.pause()
+            player.playWhenReady = false
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val index = exoPlayerItems.indexOfFirst { it.position == binding.viewPager2.currentItem }
+        if (index != -1) {
+            val player = exoPlayerItems[index].exoPlayer
+            player.playWhenReady = true
+            player.play()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (exoPlayerItems.isNotEmpty()) {
+            for (item in exoPlayerItems) {
+                val player = item.exoPlayer
+                player.stop()
+                player.clearMediaItems()
+            }
+        }
+    }
+}
